93bbd5511d5fb6af112bc88fe3780d1384a11036
ecoholan <57013643+ecoholan@users.noreply.github.com>
2020-05-04 15:31:40 -0400

    Add files via upload

--- coholan_final.py
+++ coholan_final.py
@@ -2,20 +2,17 @@
 #4/30/2020
 #GEOG 682 Final 
 
-import processing 
+import processing
 
 crimes = "S:/682/Spring20/ecoholan/Crime_Incidents_in_2017.shp"
 gunshots = "S:/682/Spring20/ecoholan/Shot_Spotter_Gun_Shots.shp"
 ward = "S:/682/Spring20/ecoholan/Ward_from_2012.shp"
 
+iface.addVectorLayer(ward, "ward", "ogr")
+iface.addVectorLayer(gunshots, "gunshots", "ogr")
 iface.addVectorLayer(crimes, "crimes", "ogr")
 
 crimes = iface.activeLayer()
-crimes.selectByExpression(" 'METHOD' = 'GUN'")
+crimes.selectByExpression("METHOD = 'GUN'")
 
-print(crimes)
-
-
-
-
-
+guns = QgsVectorFileWriter.writeAsVectorFormat(crimes, "S:/682/Spring20/ecoholan/guns.shp", onlySelected = True)
