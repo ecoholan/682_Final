8a6f8cc1921aadc1760102206fa4a1d677b9bb58
ecoholan <57013643+ecoholan@users.noreply.github.com>
2020-05-05 16:22:41 -0400

    Add files via upload

--- coholan_final.py
+++ coholan_final.py
@@ -15,4 +15,42 @@ iface.addVectorLayer(crimes, "crimes", "ogr")
 crimes = iface.activeLayer()
 crimes.selectByExpression("METHOD = 'GUN'")
 
-guns = QgsVectorFileWriter.writeAsVectorFormat(crimes, "S:/682/Spring20/ecoholan/guns.shp", onlySelected = True)
+fn = "S:/682/Spring20/ecoholan/guncrimes.shp"
+writer = QgsVectorFileWriter.writeAsVectorFormat(crimes, fn, 'utf-8', \
+driverName = 'ESRI Shapefile', onlySelected = True)
+del(writer)
+
+processing.run("qgis:joinbylocationsummary", {'INPUT':ward,'JOIN':"S:/682/Spring20/ecoholan/guncrimes.shp",'PREDICATE':1, \
+'SUMMARIES':0,'OUTPUT':"S:/682/Spring20/ecoholan/gunjoin.shp"})
+
+processing.run("qgis:intersection", {'INPUT':gunshots, 'OVERLAY':ward,\
+'OUTPUT':"S:682/Spring20/ecoholan/intersect.shp"})
+
+processing.run("qgis:joinbylocationsummary", {'INPUT':"S:/682/Spring20/ecoholan/gunjoin.shp",'JOIN':"S:682/Spring20/ecoholan/intersect.shp",'PREDICATE':1, \
+'SUMMARIES':0,'OUTPUT':"S:/682/Spring20/ecoholan/gunfinal.shp"})
+
+gunfinal = "S:/682/Spring20/ecoholan/gunfinal.shp"
+gunfinal = iface.activeLayer()
+
+pv = gunfinal.dataProvider()
+pv.addAttributes([QgsField('GC_per_10000', QVariant.Double), QgsField('SS_per_10000', QVariant.Double)])
+gunfinal.updateFields()
+
+expression1 = QgsExpression("'CCN_count'/('POP_2010'/10000)")
+expression2 = QgsExpression("'ID_count'/('POP_2010'/10000)")
+
+context = QgsExpressionContext()
+context.appendScopes(QgsExpressionContextUtils.globalProjectLayerScopes(gunfinal))
+
+with edit(gunfinal):
+    for i in gunfinal.getFeatures():
+        context.setFeature(i)
+        i['GC_per_10000'] = expression1.evaluate(context)
+        gunfinal.updateFeature(i)
+        
+with edit(gunfinal):
+    for i in gunfinal.getFeatures():
+        context.setFeature(i)
+        i['SS_per_10000'] = expression2.evaluate(context)
+        gunfinal.updateFeature(i)
+        
