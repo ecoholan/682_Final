cfc568cc6a19498d4710f8b6762aaef7c20e57a5
ecoholan <57013643+ecoholan@users.noreply.github.com>
2020-05-10 16:25:59 -0400

    Add files via upload

--- coholan_final.py
+++ coholan_final.py
@@ -5,11 +5,9 @@
 import processing
 
 crimes = "S:/682/Spring20/ecoholan/Crime_Incidents_in_2017.shp"
-gunshots = "S:/682/Spring20/ecoholan/Shot_Spotter_Gun_Shots.shp"
 ward = "S:/682/Spring20/ecoholan/Ward_from_2012.shp"
 
 iface.addVectorLayer(ward, "ward", "ogr")
-iface.addVectorLayer(gunshots, "gunshots", "ogr")
 iface.addVectorLayer(crimes, "crimes", "ogr")
 
 crimes = iface.activeLayer()
@@ -17,6 +15,18 @@ crimes.selectByExpression("METHOD = 'GUN'")
 
 fn = "S:/682/Spring20/ecoholan/guncrimes.shp"
 writer = QgsVectorFileWriter.writeAsVectorFormat(crimes, fn, 'utf-8', \
+driverName = 'ESRI Shapefile', onlySelected = True)
+del(writer)
+
+crimes.removeSelection()
+
+gunshots = "S:/682/Spring20/ecoholan/Shot_Spotter_Gun_Shots.shp"
+iface.addVectorLayer(gunshots, "gunshots", "ogr")
+gunshots = iface.activeLayer()
+gunshots.selectByExpression("year(DATETIME) = 2017")
+
+fn2 = "S:/682/Spring20/ecoholan/SS_2017.shp"
+writer = QgsVectorFileWriter.writeAsVectorFormat(gunshots, fn2, 'utf-8', \
 driverName = 'ESRI Shapefile', onlySelected = True)
 del(writer)
 
@@ -49,8 +59,11 @@ fc = count.featureCount()
 for i in range(0, fc):
     feat = count.getFeature(i)
     print(feat['NAME'], 'Gun Crimes per 10,000 people:', feat['GC_per'])
+
+SS_2017 = fn2
+iface.addVectorLayer(SS_2017, "SS_2017", "ogr")
     
-processing.run("qgis:countpointsinpolygon", {'POLYGONS':ward, 'POINTS':gunshots, 'FIELD':"NUMPOINTS", 'OUTPUT':"S:/682/Spring20/ecoholan/count2.shp"})
+processing.run("qgis:countpointsinpolygon", {'POLYGONS':ward, 'POINTS':SS_2017, 'FIELD':"NUMPOINTS", 'OUTPUT':"S:/682/Spring20/ecoholan/count2.shp"})
 
 count2 = "S:/682/Spring20/ecoholan/count2.shp"
 iface.addVectorLayer(count2, "count2", "ogr")
